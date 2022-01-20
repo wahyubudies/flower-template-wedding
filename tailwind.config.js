aa8fa9a41f9ed182d59f746a663e2dadfb5dc52b
wahyubudies <wahyubudies1@gmail.com>
2022-01-20 19:25:40 +0800

    first commit - header done

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+    './resources/**/*.{html,js}',
+    './index.html',
+    './tailwind.css',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'orange-level-1': "#E1A170",
+        'orange-level-2': "#F6E0CF",
+        'gray-level-1': "#E0E0E0",
+        'gray-level-2': "#F3F3F3",
+
+        'flower-green-level-1': '#7F7D5E',
+        'flower-green-level-2': '#E1E7D4',
+        'flower-green-level-3': '#F6F6EC',
+      },
+      backgroundImage: {
+        'hero-header': "url('../frontend/images/template-01/header.svg')"
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'gwendolyn': ['"Gwendolyn"', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
